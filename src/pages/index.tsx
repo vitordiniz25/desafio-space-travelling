4b5d910e3fcc46294447d7f22e2eb00dcb6a156d
vitordiniz25 <vitordiniz25@gmail.com>
2022-12-15 23:03:13 -0300

    estrutura inicial do projeto e configurando prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,9 +24,13 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return(
+    <main>
+      <h1>Home</h1>
+    </main>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
